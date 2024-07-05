86ffd1909c994a83c991cc1e716b6e01da8289a9
= <=>
2024-07-05 08:54:19 -0300

    Término (eu acho)

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -1,11 +1,15 @@
 from django.db import models    
 
-from core.models import Cor, Acessorio
+from core.models import Cor, Acessorio, Modelo
 
 class Veiculo (models.Model):
+    modelo = models.ForeignKey(
+        Modelo, on_delete=models.PROTECT, related_name="veiculos")
     cor = models.ForeignKey(
         Cor, on_delete=models.PROTECT, related_name="veiculos")
     ano = models.IntegerField(null=True, default=0)
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     Acessorio = models.ManyToManyField(
-        Acessorio, on_delete=models.PROTECT, related_name="veiculos")
+        Acessorio,related_name="veiculos")
+    def __str__ (self):
+        return f"{self.modelo} ({self.ano}) {self.cor}"
