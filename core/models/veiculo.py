c67c3342417edaa95d1113a7fd514eb4c46ed333
= <=>
2024-07-05 08:42:55 -0300

    Manytomany

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -7,5 +7,5 @@ class Veiculo (models.Model):
         Cor, on_delete=models.PROTECT, related_name="veiculos")
     ano = models.IntegerField(null=True, default=0)
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    Acessorio = models.ForeignKey(
+    Acessorio = models.ManyToManyField(
         Acessorio, on_delete=models.PROTECT, related_name="veiculos")
